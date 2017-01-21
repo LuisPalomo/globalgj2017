b9bac3e8ca03b72007a6dfa49f28c71ce052fa7c
KanedaXIII <mnemosineii@gmail.com>
2017-01-21 03:19:19 +0100

    Procedural Random:

---
+++ Global Game Jam 2017/Assets/Scripts/ProceduralManager.cs
@@ -0,0 +1,50 @@
+﻿using Extensions.System.Colections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralManager : MonoBehaviour {
+
+    List <GameObject> lRoom;
+
+    public GameObject lrt;
+
+    private float offSetP;
+
+	// Use this for initialization
+	void Start () {
+       Renderer rend = GetComponent<Renderer>();
+       Vector3 v3Rend=Vector3.forward*rend.bounds.size.z;
+        
+       Vector3 tMid = (rend.bounds.max+rend.bounds.min)*0.5f;
+        
+        tMid = tMid + v3Rend;
+      
+        Instantiate(lrt,tMid,Quaternion.identity);
+        //Vector3  = rend.bounds.SqrDistance(tMid*v3Rend);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void generationLevel(int nLevel){
+
+        int i = 0;
+
+
+
+        while ( i<nLevel)
+        {
+            GameObject tempRoom = lRoom.RandomItem();
+
+            //if(tempRoom.GetComponent<ProceduralRoom>().name)
+           
+        }
+
+       
+
+
+        }
+}
---
+++ Global Game Jam 2017/Assets/Scripts/ProceduralRoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralRoom : MonoBehaviour {
+
+    public string nameRoom;
+
+    public int typeRoom;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
